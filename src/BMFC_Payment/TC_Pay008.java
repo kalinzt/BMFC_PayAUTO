eca264ebabb3a7b018b31ffc0990c25b9b544fb4
kalinzt <kalinzt@gmail.com>
2018-07-09 16:27:59 +0900

    BMFC_Payment Automation

--- src/BMFC_Payment/TC_Pay008.java
+++ src/BMFC_Payment/TC_Pay008.java
@@ -1,19 +1,19 @@
 package BMFC_Payment;
 
-import org.junit.After;
+import static org.junit.Assert.fail;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.After;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
-
-import static org.junit.Assert.fail;
 
 public class TC_Pay008 {
     private WebDriver driver;
@@ -31,6 +31,7 @@ public class TC_Pay008 {
     public void p_008() throws Exception {
         driver.get("https://www.baeminchan.com");
 
+        //사전조건
         driver.findElement(By.xpath("//*[@id=\"lnb\"]/ul/li[1]/a")).click(); //GNB 로그인 버튼 클릭
         driver.findElement(By.xpath("//*[@id=\"member_id\"]")).sendKeys("kalinzt@gmail.com"); //아이디 입력
         driver.findElement(By.xpath("//*[@id=\"pwd\"]")).sendKeys("worud1029"); //패스워드 입력
@@ -45,12 +46,20 @@ public class TC_Pay008 {
         driver.findElement(By.xpath("/html/body/div[3]/div[1]/div/div/div[2]/div[2]/button")).click(); //주문하기 클릭
         Thread.sleep(3000);
 
+        //보유 포인트 전액 사용하기
+        driver.findElement(By.xpath("//*[@id=\"milage_prc\"]")).clear();
+        String a = driver.findElement(By.xpath("//*[@id=\"ordFrm\"]/div[1]/div[2]/div/dl[2]/dt[1]/p/span")).getText();
+        driver.findElement(By.xpath("//*[@id=\"milage_prc\"]")).sendKeys(a);
+        driver.findElement(By.xpath("//*[@id=\"ordFrm\"]/div[1]/div[2]/div/dl[2]/dd[1]/button")).click();
+        Thread.sleep(1000);
+
         //주문필수 스크립트(상품조건별로 노출되는 체크박스 제어)
-        //카드결제
+        //불필요한 물류데이터를 방지하기 위해 무통장 결제만 사용함(라이브기준)
         if(driver.findElement(By.xpath("//*[@id=\"order_delivery_memo\"]/td")).getText().equalsIgnoreCase("새벽배송")) {
             driver.findElement(By.xpath("//*[@id=\"door_number\"]/td/input")).sendKeys("2325*");
             return;
         } //새벽배송 비번입력
+        driver.findElement(By.xpath("//*[@id=\"pay_type4\"]")).click(); //무통장 클릭
         if(driver.findElement(By.xpath("//*[@id=\"ordFrm\"]/ul/li[1]")).getText().equalsIgnoreCase("전체동의")) {
             driver.findElement(By.xpath("//*[@id=\"ordFrm\"]/ul/li[1]/span/label/strong")).click(); //구매조건 동의 외 체크박스 존재시 전체동의 체크
         } else {
@@ -59,40 +68,18 @@ public class TC_Pay008 {
         driver.findElement(By.xpath("//*[@id=\"order1\"]/button")).click(); //결제하기
         Thread.sleep(4000);
 
-        //카드결제 팝업닫기
-        String winHandleBefore = driver.getWindowHandle();
-        for(String winHandle : driver.getWindowHandles()){ driver.switchTo().window(winHandle); }
-        driver.close();
-        driver.switchTo().window(winHandleBefore);
-        Thread.sleep(5000);
-
-        //PG module 제어 (카드결제)
+        //PG module 제어 (무통장)
         driver.switchTo().frame(1);
         driver.findElement(By.cssSelector("#assentAll")).click(); //PG전체동의
-        driver.findElement(By.cssSelector("#main_agreed_info > div > p.progressBtn > a")).click(); //PG다음1
-        driver.findElement(By.cssSelector("#CARDTYPE0")).click(); //카드선택(우리)
-        driver.findElement(By.cssSelector("#chk_payinput")).click(); //구매내역 동의
-
-        String parentHandle = driver.getWindowHandle(); // 현재 윈도우를 부모핸들로 지정
+        driver.findElement(By.cssSelector("#main_agreed_info > div > p.progressBtn > a")).click(); //PG다음
+        driver.findElement(By.cssSelector("#BANKCODE3")).click(); //은행 클릭
+        driver.findElement(By.cssSelector("#LGD_ACCOUNTOWNER")).sendKeys("김대웅"); //입금자명 입력
+        driver.findElement(By.cssSelector("#LGD_CASHRECEIPTUSE0")).click(); //현금영수증 미발행
+        driver.findElement(By.cssSelector("#chk_payinput")).click(); //구매동의
+        driver.findElement(By.cssSelector("#LGD_NEXT > a")).click(); //PG다음1
+        Thread.sleep(2000);
         driver.findElement(By.cssSelector("#LGD_NEXT > a")).click(); //PG다음2
-        for (String winHandle : driver.getWindowHandles()) {
-            driver.switchTo().window(winHandle); // 신규 윈도우를 찾으면 해당 윈도우로 핸들 스위칭
-        }
-        driver.findElement(By.cssSelector("#container > div.menu_box.last > div.menu_btn > a > span")).click(); //현대카드 일반결제
-
-        driver.findElement(By.cssSelector("#cardno1")).click();
-        driver.findElement(By.cssSelector("#nppfs-keypad-cardno1 > div > div.kpd-group.number > div:nth-child(8) > img")).click();
-        driver.findElement(By.cssSelector("#nppfs-keypad-cardno1 > div > div.kpd-group.number > div:nth-child(7) > img")).click();
-        driver.findElement(By.cssSelector("#nppfs-keypad-cardno1 > div > div.kpd-group.number > div:nth-child(7) > img")).click();
-        driver.findElement(By.cssSelector("#nppfs-keypad-cardno1 > div > div.kpd-group.number > div:nth-child(14) > img")).click();
-
-
-        driver.findElement(By.cssSelector("#cardno2")).sendKeys("2886");
-        driver.findElement(By.cssSelector("#cardno3")).sendKeys("9745");
-        driver.findElement(By.cssSelector("#cardno4")).sendKeys("7004");
-
-
-
+        Thread.sleep(2000);
 
         //주문취소
         driver.findElement(By.xpath("//*[@id=\"orderFinish\"]/div/div/button[1]/span")).click(); //주문배송조회 클릭
@@ -105,7 +92,7 @@ public class TC_Pay008 {
         Thread.sleep(2000);
         Alert alert2 = driver.switchTo().alert();
         alert2.accept();
-        Thread.sleep(1000);
+        Thread.sleep(2000);
         driver.findElement(By.xpath("//*[@id=\"header_wrap\"]/div/h1/a/img")).click(); //메인화면가기
         Thread.sleep(1000);
         driver.findElement(By.xpath("//*[@id=\"lnb\"]/ul/li[1]/a")).click(); //로그아웃
