725c8c2dd7e7e80381b82db31c502689da92b53a
kalinzt <kalinzt@gmail.com>
2018-07-12 17:45:41 +0900

    BMFC_Payment Automation

--- src/BMFC_Payment/TC_Pay010.java
+++ src/BMFC_Payment/TC_Pay010.java
@@ -15,7 +15,7 @@ import java.util.concurrent.TimeoutException;
 
 import static org.junit.Assert.fail;
 
-public class TC_Pay007 {
+public class TC_Pay010 {
     private WebDriver driver;
     private StringBuffer verificationErrors = new StringBuffer();
 
@@ -28,47 +28,60 @@ public class TC_Pay007 {
     }
 
     @Test
-    public void p_007() throws Exception {
-        driver.get("http://baeminchan.com");
+    public void p_010() throws Exception {
+        driver.get("https://www.baeminchan.com");
 
+        //사전조건
         driver.findElement(By.xpath("//*[@id=\"lnb\"]/ul/li[1]/a")).click(); //GNB 로그인 버튼 클릭
         driver.findElement(By.xpath("//*[@id=\"member_id\"]")).sendKeys("kalinzt@gmail.com"); //아이디 입력
         driver.findElement(By.xpath("//*[@id=\"pwd\"]")).sendKeys("worud1029"); //패스워드 입력
         driver.findElement(By.xpath("//*[@id=\"login\"]/div[1]/form/fieldset/button")).click(); //로그인 버튼 클릭
-        driver.findElement(By.xpath("//*[@id=\"search_str\"]")).sendKeys("집밥"); //집밥 검색어 입력
+        driver.findElement(By.xpath("//*[@id=\"search_str\"]")).sendKeys("반찬"); //집밥 검색어 입력
         driver.findElement(By.xpath("//*[@id=\"header_wrap\"]/div/form/button")).click(); // 검색버튼 클릭
         driver.findElement(By.xpath("//*[@id=\"products\"]/li[1]/div/a")).click(); // 검색결과 첫번째 상품 클릭
         driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div[2]/div/form/fieldset/div/dl/dd/div/label/input")).clear();
-        driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div[2]/div/form/fieldset/div/dl/dd/div/label/input")).sendKeys("5");
+        driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div[2]/div/form/fieldset/div/dl/dd/div/label/input")).sendKeys("10");
         driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div[2]/div/form/fieldset/button")).click(); //장바구니담기
         driver.findElement(By.xpath("//*[@id=\"lnb\"]/ul/li[6]/a")).click(); //GNB장바구니 클릭
+        Thread.sleep(1000);
         driver.findElement(By.xpath("//*[@id=\"btn_select_receipt_date\"]")).click(); //희망배송일 선택
         driver.findElement(By.xpath("/html/body/div[3]/div[1]/div/div/div[2]/div[2]/button")).click(); //주문하기 클릭
-        Thread.sleep(2000);
+        Thread.sleep(3000);
 
         //쿠폰선택
-        driver.findElement(By.cssSelector("#ordFrm > table.tb_order_style > tbody > tr > td:nth-child(8) > button")).click(); //쿠폰선택
-        driver.findElement(By.cssSelector("#coupon-list-tbl > tbody > tr:nth-child(1) > td:nth-child(1) > span > label")).click(); //보유한쿠폰선택
-        driver.findElement(By.cssSelector("#coupon_apply")).click();//쿠폰 적용
-        Thread.sleep(1000);
-        Alert alert0 = driver.switchTo().alert();
-        alert0.accept();
-        Thread.sleep(1000);
+        //driver.findElement(By.cssSelector("#ordFrm > table.tb_order_style > tbody > tr > td:nth-child(8) > button")).click(); //쿠폰선택
+        //driver.findElement(By.cssSelector("#coupon-list-tbl > tbody > tr:nth-child(1) > td:nth-child(1) > span > label")).click(); //보유한쿠폰선택
+        //driver.findElement(By.cssSelector("#coupon_apply")).click();//쿠폰 적용
+        //Thread.sleep(1000);
+        //Alert alert0 = driver.switchTo().alert();
+        //alert0.accept();
+        //Thread.sleep(1000);
 
-        //보유포인트 전액 사용하기(포인트 잔액 충족하지 않음)
+        //보유 포인트 전액 사용하기
+        //getText로 받은 문자열을 정수로 변환 및 천단위 콤마 제거
         driver.findElement(By.xpath("//*[@id=\"milage_prc\"]")).clear();
-        String a = driver.findElement(By.xpath("//*[@id=\"total_order_price_pay\"]")).getText();
-        driver.findElement(By.xpath("//*[@id=\"milage_prc\"]")).sendKeys(a);
+
+        String a = driver.findElement(By.xpath("//*[@id=\"ordFrm\"]/div[1]/div[2]/div/dl[2]/dt[1]/p/span")).getText(); //보유포인트
+        a = a.replaceAll(",", "");
+        int ordFrm = Integer.parseInt(a);
+        //System.out.println(ordFrm);
+
+        String b = driver.findElement(By.xpath("//*[@id=\"total_order_price_pay\"]")).getText(); //결제총액
+        b = b.replaceAll(",", "");
+        int totalPay = Integer.parseInt(b);
+        //System.out.println(totalPay);
+
+        if (ordFrm > totalPay) {
+            driver.findElement(By.xpath("//*[@id=\"milage_prc\"]")).sendKeys(b);
+        } else {
+            driver.findElement(By.xpath("//*[@id=\"milage_prc\"]")).sendKeys(a);
+        }
+
         driver.findElement(By.xpath("//*[@id=\"ordFrm\"]/div[1]/div[2]/div/dl[2]/dd[1]/button")).click();
         Thread.sleep(1000);
-        Alert alert1 = driver.switchTo().alert();
-        alert1.accept();
-        Thread.sleep(1000);
-        driver.findElement(By.xpath("//*[@id=\"milage_prc\"]")).sendKeys("100");
-        driver.findElement(By.xpath("//*[@id=\"ordFrm\"]/div[1]/div[2]/div/dl[2]/dd[1]/button")).click();
 
         //주문필수 스크립트(상품조건별로 노출되는 체크박스 제어)
-        //불필요한 물류데이터를 방지하기 위해 무통장 결제만 사용함
+        //불필요한 물류데이터를 방지하기 위해 무통장 결제만 사용함(라이브기준)
         if(driver.findElement(By.xpath("//*[@id=\"order_delivery_memo\"]/td")).getText().equalsIgnoreCase("새벽배송")) {
             driver.findElement(By.xpath("//*[@id=\"door_number\"]/td/input")).sendKeys("2325*");
             return;
@@ -101,16 +114,25 @@ public class TC_Pay007 {
         driver.findElement(By.xpath("//*[@id=\"order_subscription\"]/table/tbody/tr[1]/td[3]/p[1]/a")).click(); //첫주문내역 클릭
         driver.findElement(By.xpath("//*[@id=\"order_detail\"]/table[2]/tbody/tr[4]/td/a[1]")).click(); //결제취소 클릭
         Thread.sleep(2000);
+        Alert alert1 = driver.switchTo().alert();
+        alert1.accept();
+        Thread.sleep(2000);
         Alert alert2 = driver.switchTo().alert();
         alert2.accept();
         Thread.sleep(2000);
-        Alert alert3 = driver.switchTo().alert();
-        alert3.accept();
-        Thread.sleep(3000);
         driver.findElement(By.xpath("//*[@id=\"header_wrap\"]/div/h1/a/img")).click(); //메인화면가기
         Thread.sleep(1000);
         driver.findElement(By.xpath("//*[@id=\"lnb\"]/ul/li[1]/a")).click(); //로그아웃
 
+        // text check
+        //if ("주문이 정상적으로 완료되었습니다!".equals(driver.findElement(By.xpath("//*[@id=\"orderFinish\"]/div/h2")).getText())) {
+        //System.out.println("PASS");
+        //assertTrue(true);
+        //return;
+        //} else {
+        //System.out.println("FAIL");
+        //assertTrue(false);
+        //}
 
     }
 
@@ -129,3 +151,5 @@ public class TC_Pay007 {
         return true;
     }
 }
+
+
